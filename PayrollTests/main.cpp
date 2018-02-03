db222b3332b9c2cefe02a9546d925a35602fe5e1
anymuse <anymuse@gmail.com>
2018-02-03 23:38:39 +0800

    Initial commit

---
+++ PayrollTests/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by anymuse on 2018/2/3.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include "lib/catch.hpp"
+
+//
+//unsigned int Factorial( unsigned int number ) {
+//    return number <= 1 ? number : Factorial(number-1)*number;
+//}
+//
+//TEST_CASE("Factorial are computed", "[factorial]") {
+//    REQUIRE(Factorial(1) == 1);
+//}
